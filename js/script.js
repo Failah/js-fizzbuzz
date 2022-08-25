7ff1963cdcc6b18b1f5fb43f0a22f94c5fd65011
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-25 14:22:07 +0200

    Created the programm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('JS OK!')
+
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 !== 0) {
+        console.log('Fizz');
+    } else if (index % 5 === 0 && index % 3 !== 0) {
+        console.log('Buzz');
+    } else if (index % 5 === 0 && index % 3 === 0) {
+        console.log('FizzBuzz');
+    } else {
+        console.log(index);
+    }
+}
